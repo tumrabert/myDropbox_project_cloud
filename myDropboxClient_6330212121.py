8a315f2b56ec3fad2258b6f5ac11a1fba57aa4d3
tumrabert <tanakit.p@ayodiacompany.com>
2024-02-20 23:17:08 +0700

    My Dropbox with full features

--- myDropboxClient_6330212121.py
+++ myDropboxClient_6330212121.py
@@ -1,10 +1,26 @@
-import boto3
-import json
 import requests
 import base64
 import os
 from dotenv import load_dotenv
+import hashlib
+
+
+# Load environment variables from a .env file
+load_dotenv()
+
 API_GATEWAY = os.getenv("API_GATEWAY") + '/act5v2/api/v1'
+def hash_password(password):
+    # Convert the password to bytes
+    password_bytes = password.encode('utf-8')
+    # Create a SHA-256 hash object
+    sha256 = hashlib.sha256()
+    # Update the hash object with the password bytes
+    sha256.update(password_bytes)
+    # Get the hashed password in hexadecimal format
+    hashed_password = sha256.hexdigest()
+    return hashed_password
+
+
 def get_file_content_base64(file_path):
     """Reads a file and returns its base64 encoded content."""
     try:
@@ -14,10 +30,12 @@ def get_file_content_base64(file_path):
     except Exception as e:
         print(f"Error reading file {file_path}: {e}")
         return None
+    
+
+
 def make_api_request(method, endpoint, data=None):
     """Makes an API request and returns the response status code and content."""
-    url = f"{GATEWAY}/{endpoint}"
-    print(url)
+    url = f"{API_GATEWAY}/{endpoint}"
     headers = {'Content-Type': 'application/json'}
     try:
         response = requests.request(method, url, headers=headers, json=data)
@@ -27,16 +45,16 @@ def make_api_request(method, endpoint, data=None):
         print(f"Error making API request: {e}")
         return None, None
 
-def put(file_name):
+def put(file_name,owner):
     """Uploads a file to the API."""
     file_path = f"./{file_name}"
     file_content = get_file_content_base64(file_path)
 
     if not file_content:
-        return
+        return False
 
     data = {
-        'owner': "p",  
+        'owner': owner,
         'file_name': file_name,
         'file': file_content
     }
@@ -44,64 +62,212 @@ def put(file_name):
     status_code, _ = make_api_request('PUT', 'put', data)
     if status_code == 200:
         print("File uploaded successfully.")
+        return True
+    else:
+        print("Error uploading file.")
+        return False
 
-def view(owner="p"):
-    # Assuming 'view' can be called with a GET request without parameters
+def view(owner):
+    """Lists files associated with the specified owner."""
     if not owner:
         print("Owner's name cannot be empty.")
-        return
+        return False
+
     data = {'owner': owner}
     status_code, files = make_api_request('GET', 'view', data)
-    print(files,type(files))
     if status_code == 200 and files:
         for file in files['files']:
             print(file)
+        return True
     else:
         print("No files found for this owner.")
+        return False
 
 def get(file_name, owner):
     """Downloads a file from the API."""
     data = {'owner': owner, 'file_name': file_name}
-    response = make_api_request('GET', 'get', data)
+    status_code, response_data = make_api_request('GET', 'get', data)
 
-    if not response:
-        return
-    json_data = response[1]
-
-    # Access the 'file_url' key in the JSON data
-    file_url = json_data.get('file_url')
-    if file_url:
-        print("Downloading file...")
-        response = requests.get(file_url)
-        with open(file_name, 'wb') as file:
-            file.write(response.content)
-        print("File downloaded successfully.")
+    if status_code == 200 and response_data:
+        file_url = response_data.get('file_url')
+        if file_url:
+            print("Downloading file...")
+            response = requests.get(file_url)
+            with open(file_name, 'wb') as file:
+                file.write(response.content)
+            print("File downloaded successfully.")
+            return True
+        else:
+            print("File not found.")
+            return False
     else:
-        print("File not found.")
+        print("Error fetching file.")
+        return False
+    
+def createUser(user,password):
+    print(f"Creating a new user => {user}") 
+    hashed_pwd = hash_password(password)
+    data = {'username': user, 'password': hashed_pwd}
+    status_code, respond= make_api_request('POST', 'user_auth/register', data)
+    if status_code == 200:
+        print(f"User {user} created successfully.")
+    else:
+        print(respond)
+        print(f"User {user} already exists.")
+def login(user,password):
+    hash_pw=hash_password(password)
+    data = {'username':user,'password':hash_pw}
+    status_code, response_data = make_api_request('POST', 'user_auth/login', data)
+    if status_code == 200:
+        print("Login successfully.")
+        return True
+    else:
+        print("Login Failed.")
+        print(response_data)
+        return False
+    
+def check_user_exists(username):
+    data = {'username': username}
+    try:
+        status_code, response = make_api_request('GET', 'user_auth/checkuser', data)
+        if status_code == 200:
+            return True, "User exists"
+        elif status_code == 404:
+            return False, "User does not exist"
+        else:
+            return False, f"Error: {response}"
+    except Exception as e:
+        return False, f"Error: {e}"
 
-def select_function(ip):
+def share(owner,filename,userToShare):
+
+    if(check_user_exists(userToShare)):
+        data = {'owner': owner, 'filename': filename, 'shareTo': userToShare}
+        status_code,response = make_api_request('POST', 'share', data)
+        if status_code == 200:
+            print(f"File {filename} shared with {userToShare} successfully.")
+        else:
+            print("Failed to shared file")
+            print(response)
+            return False
+    else:
+        print(f"{userToShare} is not Existed")
+        return False
+    return True
+
+def check_download_permission(username, file_name, owner):
+    """Checks if the user has permission to download the file."""
+    data = {'owner': username}
+    status_code, response = make_api_request('GET', 'view', data)
+    if status_code == 200 and response:
+        files = response.get('files', [])
+        shared_files = response.get('sharefile', [])
+        if files:
+            for file in files:
+                if file['Key'] == file_name and file['owner'] == owner:
+                    return True
+        if shared_files:
+            for file in shared_files:
+                if file['Key'] == file_name and file['owner'] == owner:
+                    return True
+    return False
+
+
+'''def select_function(ip,username):
+    global USER
     if(ip[0]=='put'):
-        put(ip[1])
+        print("OK") if put(ip[1],username) else print("put error")
     elif(ip[0]=='view'):
-        view()
+        print("OK") if view(username) else print("view error")
     elif(ip[0]=='get'):
-        get(ip[1],ip[2])
+        if(check_download_permission(username, ip[1],ip[2])):
+            print("OK") if get(ip[1],ip[2]) else print("get error")
+        else:
+            print("You do not have permission to download this file.")        
+    elif(ip[0]=='newuser'):
+        print("OK") if createUser(ip[1],ip[3]) else print("Create User Error")
+    elif(ip[0]=='login'):
+        if login(ip[1],ip[2]):
+            USER=username
+            print("OK") 
+        else:
+            USER=""
+            print("Login user invalid username or password")
+    elif(ip[0]=='share'):
+        print("OK") if share(username,ip[1],ip[2]) else print("share error")
     else:
         print("Please input correct command")
-        return
-    print("OK")
-        
+        return'''
+    
+def main():
+    print("Welcome to myDropbox Application")
+    print("===================================")
+    print("Available commands:")
+    print("newuser username password password")
+    print("login username password")
+    print("put filename")
+    print("get filename owner")
+    print("share filename userToshare")
+    print("view")
+    print("logout")
+    print("quit")
+    print("===================================")
+    username = None
+    while True:
+        print("MY USER",username)
+        user_input = input(">> ").split()
+        command = user_input[0]
+        if command == 'quit':
+            break
 
-print("welcome to myDropbox Application")
-print('''======================================================
-Please input command (newuser username password password, login 
-username password, put filename, get filename, view, or logout). 
-If you want to quit the program just type quit.
-======================================================''')
-ip=input().split()
+        if command not in ['newuser', 'login', 'put', 'get', 'view', 'logout', 'share']:
+            print("Please input a correct command.")
+            continue
 
-while(ip[0]!='quit'):
-    ip=input().split()
-    select_function(ip)
-print("======================================================")
+        if command == 'logout':
+            username = None
+            print("You're Logged out.")
+            continue
 
+        if username:  # Already logged in
+            allowed_commands = ['newuser', 'put', 'get', 'view', 'logout', 'share']
+            if user_input[0] in allowed_commands:
+                #select_function(user_input, USER)
+                if(command=='put'):
+                    print("OK") if put(user_input[1],username) else print("put error")
+                elif(command=='view'):
+                    print("OK") if view(username) else print("view error")
+                elif(command=='get'):
+                    if(check_download_permission(username, user_input[1],user_input[2])):
+                        print("OK") if get(user_input[1],user_input[2]) else print("get error")
+                    else:
+                        print("You do not have permission to download this file.")        
+                elif(command=='newuser'):
+                    print("OK") if createUser(user_input[1],user_input[3]) else print("Create User Error")
+                elif(command=='share'):
+                    print("OK") if share(username,user_input[1],user_input[2]) else print("share error")
+                else:
+                    print("Please input correct command")
+                    return
+            else:
+                print("You're not allowed to use this command1.")
+        else:
+            allowed_commands = ['newuser', 'login']
+            if user_input[0] in allowed_commands:
+                #select_function(user_input, USER)
+                if(command=='newuser'):
+                    print("OK") if createUser(user_input[1],user_input[3]) else print("Create User Error")
+                elif(command=='login'):
+                    if login(user_input[1],user_input[2]):
+                        username=user_input[1]
+                        print("OK") 
+                    else:
+                        print("Login user invalid username or password")
+                else:
+                    print("Please input correct command")
+                    return
+            else:
+                print("You're not allowed to use this command2.")
+
+if __name__ == "__main__":
+    main()
--- myDropbox_6330212121.py
+++ myDropbox_6330212121.py
@@ -1,75 +1,414 @@
-import json
 import boto3
+import json
+import base64
 import os
+import uuid
+from datetime import datetime
+from boto3.dynamodb.conditions import Key
+BASE_PATH = '/act5v2/api/v1'
+GET_PATH = f'{BASE_PATH}/get'
+PUT_PATH = f'{BASE_PATH}/put'
+VIEW_PATH = f'{BASE_PATH}/view'
+CHECKUSER_PATH = f'{BASE_PATH}/user_auth/checkuser'
+LOGIN_PATH = f'{BASE_PATH}/user_auth/login'
+REGISTER_PATH = f'{BASE_PATH}/user_auth/register'
+SHARE_PATH =f'{BASE_PATH}/share'
+BUCKET_NAME = os.getenv('s3_BUCKET_NAME')
 s3 = boto3.client('s3')
-BUCKET_NAME = os.environ['s3_BUCKET_NAME']
+dynamodb = boto3.client('dynamodb')
 def lambda_handler(event, context):
-    
-    http_method = event["httpMethod"]
-    endpoint = event["path"]
-
-    if http_method == "GET" and endpoint == "/act5v2/api/v1/get":
+    """Handles API requests for file operations."""
+    try:
+        path = event['path']
         body = json.loads(event["body"])
-        filename = body.get("filename")
-        return getMethod(filename)
-    elif http_method == "PUT" and endpoint == "/act5v2/api/v1/put":
-        body = json.loads(event["body"])
-        filename = body.get("filename")
-        file_object = body.get("file_object")
-        return putMethod(filename, file_object)
-    elif http_method == "GET" and endpoint == "/act5v2/api/v1/view":
-        return viewMethod()
-    else:
-        return {
-            "statusCode": 404,
-            "body": json.dumps({"message": "Endpoint not found"})
-        }
 
-def getMethod(filename):
-    try:
-        response = s3.get_object(Bucket=BUCKET_NAME, Key=filename)
-        file_content = response['Body'].read().decode('utf-8')
-        return {
-            "statusCode": 200,
-            "body": file_content
-        }
-    except Exception as e:
-        return {
-            "statusCode": 500,
-            "body": json.dumps({"message": str(e)})
-        }
-
-def putMethod(filename, file_object):
-    try:
-        s3.put_object(Bucket=BUCKET_NAME, Key=filename, Body=file_object)
-        return {
-            "statusCode": 200,
-            "body": json.dumps({"message": "File uploaded successfully"})
-        }
-    except Exception as e:
-        return {
-            "statusCode": 500,
-            "body": json.dumps({"message": str(e)})
-        }
-
-def viewMethod():
-    try:
-        response = s3.list_objects_v2(Bucket=BUCKET_NAME)
-        files_info = []
-        for obj in response.get('Contents', []):
-            file_info = {
-                "filename": obj['Key'],
-                "lastModifiedDate": obj['LastModified'].isoformat(),
-                "size": obj['Size'],
-                "owner": obj['Owner']['DisplayName']
+        if path == PUT_PATH:
+            return _handle_put_request(body)
+        elif path == GET_PATH:
+            return _handle_get_request(body)
+        elif path == VIEW_PATH:
+            return _handle_view_request(body)
+        elif path == CHECKUSER_PATH:
+            return _handle_chkuser_request(body)
+        elif path == REGISTER_PATH:
+            return _handle_register_request(body)
+        elif path == LOGIN_PATH:
+            return _handle_login_request(body)
+        elif path == SHARE_PATH:
+            return _handle_share_request(body)
+        else:
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'error': 'Invalid path'})
             }
-            files_info.append(file_info)
+    except Exception as e:
+        print(f"Error handling lambda request: {e}")
         return {
-            "statusCode": 200,
-            "body": json.dumps(files_info)
+            'statusCode': 500,
+            'body': json.dumps({'error': 'Internal server error'})
+        }
+
+def _get_object_key(owner, file_name):
+    """Constructs the object key for a file based on owner and name."""
+    return f"{owner}/{file_name}"
+
+def list_files_for_owner(owner):
+    
+    files = []
+    prefix = f"{owner}/"  # Assuming folder names are based on owners
+    
+    response = s3.list_objects_v2(Bucket=BUCKET_NAME, Prefix=prefix)
+    for obj in response.get('Contents', []):
+        dateTime = obj['LastModified']
+        stringTime = dateTime.strftime('%Y-%m-%d %H:%M:%S')
+        files.append({
+            "Key":obj['Key'].split('/')[-1], 
+            "Size": obj['Size'], 
+            "LastModified": stringTime,
+            "owner": owner
+        })
+    return files[1:]
+
+
+def get_file_url(owner, file_name):
+    """Generates a presigned URL for downloading a file."""
+    files = list_files_for_owner(owner)
+    file_key = _get_object_key(owner, file_name)
+    
+    if file_name not in files:
+        return None
+    file_key = f'{owner}/{file_name}'
+    try:
+        file_url = s3.generate_presigned_url('get_object', Params={'Bucket': BUCKET_NAME, 'Key': file_key}, ExpiresIn=3600)
+        return file_url
+    except Exception as e:
+        print(f"Error generating URL for {file_key}: {e}")
+        return None
+
+
+def create_folder(folder_path):
+    """Creates a folder in the bucket, ignoring errors if it already exists."""
+    try:
+        s3.put_object(Bucket=BUCKET_NAME, Key=folder_path)
+    except Exception as e:
+        print(f"Error creating folder {folder_path}: {e}")
+
+
+def upload_file_to_s3(file_content, file_key):
+    """Uploads a file to S3 with the specified key."""
+    try:
+        s3.put_object(Body=file_content, Bucket=BUCKET_NAME, Key=file_key)
+    except Exception as e:
+        print(f"Error uploading file {file_key}: {e}")
+
+
+
+
+
+def _handle_put_request(body):
+    """Handles PUT requests for uploading files."""
+    try:
+        owner = body.get('owner')
+        file_name = body.get('file_name')
+        file = body.get('file')
+
+        if not all([owner, file_name, file]):
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'error': 'Missing required fields'})
+            }
+
+        folder_path = _get_object_key(owner, '')
+        create_folder(folder_path)
+
+        file_content = base64.b64decode(file)
+        file_key = _get_object_key(owner, file_name)
+        upload_file_to_s3(file_content, file_key)
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'post': 'OK'})
         }
     except Exception as e:
+        print(f"Error handling PUT request: {e}")
         return {
-            "statusCode": 500,
-            "body": json.dumps({"message": str(e)})
+            'statusCode': 500,
+            'body': json.dumps({'error': 'Internal server error'})
         }
+
+
+def _handle_get_request(body):
+    """Handles GET requests for download file URLs."""
+    try:
+        file_name = body.get('file_name')
+        owner = body.get('owner')
+
+        if not all([file_name, owner]):
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'error': 'Missing required fields'})
+            }
+
+        file_url = get_file_url(owner, file_name)
+        if file_url:
+            return {
+                'statusCode': 200,
+                'body': json.dumps({'file_url': file_url})
+            }
+        else:
+            return {
+                'statusCode': 404,
+                'body': json.dumps({'error': 'File not found'})
+            }
+    except Exception as e:
+        print(f"Error handling GET request: {e}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': 'Internal server error'})
+        }
+
+
+def _handle_view_request(body):
+    """Handles VIEW requests for listing files for an owner."""
+    try:
+        owner = body.get('owner')
+
+        if not owner:
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'error': 'Missing owner field'})
+            }
+
+        files = list_files_for_owner(owner)
+        sharefile = list_files_shared_with_user(owner)
+        file_lists = convert_sharefile_to_list_files(sharefile)
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'files': files, 'sharefile': file_lists})
+        }
+    except Exception as e:
+        print(f"Error handling VIEW request: {e}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': 'Internal server error'})
+        }
+    
+def _handle_chkuser_request(body):
+    """Handles VIEW requests for listing files for an owner."""
+    try:
+        username = body.get('username')
+
+        if not username:
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'error': 'Missing username field'})
+            }
+        # Query dynamodb table to check if the username exists
+        response = dynamodb.get_item(
+            TableName='userTable',
+            Key={
+                'username': {'S': username}
+            }
+        )
+        if 'Item' in response:
+            return {
+                'statusCode': 200,
+                'body': json.dumps({'message': f'{username} found in table'})
+            }
+        else:
+            return {
+                'statusCode': 404,
+                'body': json.dumps({'error': f'{username} not found in table'})
+            }
+    except Exception as e:
+        print(f"Error handling VIEW request: {e}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': 'Internal server error'})
+        }
+    
+def _handle_register_request(body):
+    """Handles REGISTER requests for creating a new user."""
+    try:
+        username = body.get('username')
+        password = body.get('password')
+        if not all([username, password]):
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'error': 'Missing required fields'})
+            }
+
+        if (register_user(username, password)):
+            return {
+                'statusCode': 200,
+                'body': json.dumps({'register': "OK"})
+            }
+        else:
+            return {
+                'statusCode': 409,
+                'body': json.dumps({'error': 'User already exists'})
+            }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': 'Error handling REGISTER request'})
+        }
+    
+def register_user(username, password):
+    """Registers a new user in the database."""
+    dynamo = boto3.resource('dynamodb')
+    try:
+        table = dynamo.Table('userTable')
+        response = table.put_item(
+        Item={
+            'username': username,
+            'password': password
+        },
+        ConditionExpression='attribute_not_exists(username)'
+    )
+        return True
+    except Exception as e:
+        return False 
+
+
+def _handle_login_request(body):
+    """Handles LOGIN requests for logging in a user."""
+    try:
+        username = body.get('username')
+        password = body.get('password')
+        
+        if not all([username, password]):
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'error': 'Missing required fields'})
+            }
+
+        # Implement login and session management
+        response = login_user(username, password)
+        if response:
+            return {
+                'statusCode': 200,
+                'body': json.dumps({'login': 'OK'})
+            }
+        else:
+            return {
+                'statusCode': 401,
+                'body': json.dumps({'error': 'Invalid credentials'})
+            }
+    except Exception as e:
+        print(f"Error handling LOGIN request: {e}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': 'Error handling LOGIN request'})
+        }
+
+def login_user(username, password):
+  
+    # Instantiate a table resource object
+    dynamo = boto3.resource('dynamodb')
+    table = dynamo.Table('userTable')
+    
+    # Get the user from the database
+    response = table.get_item(Key={'username': username})
+
+    # Check if user exists and password matches
+    if 'Item' in response:
+        user = response['Item']
+        if user['password'] == password:
+            print("Login successful")
+            return True
+        else:
+            print("Incorrect password")
+            return False
+    else:
+        print("User does not exist")
+        return False
+def _handle_share_request(body):
+    """Handles SHARE requests for sharing a file with another user."""
+    try:
+        owner = body.get('owner')
+        filename = body.get('filename')
+        shareTo = body.get('shareTo')
+
+        if not all([owner, filename, shareTo]):
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'error': 'Missing required fields'})
+            }
+
+        # Implement sharing logic
+        if (owner == shareTo):
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'error': 'Cannot share with yourself'})
+            }
+        try:
+            if sharefile(owner, filename, shareTo):
+                return {
+                    'statusCode': 200,
+                    'body': json.dumps({'share': 'OK'})
+                }
+            else:
+                return {
+                    'statusCode': 400,
+                    'body': json.dumps({'error': 'Owner user does not exist'})
+                }
+        except Exception as e:
+            return {
+                'statusCode': 500,
+                'body': json.dumps({'error': f'Error sharing file {e}'})
+            }
+    except Exception as e:
+        print(f"Error handling SHARE request: {e}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': 'Error handling SHARE request'})
+        }
+def sharefile(owner, filename, shareTo):
+    share_id = str(uuid.uuid4())
+    dynamo = boto3.resource('dynamodb')
+    table = dynamo.Table('shareTable')
+    usertable = dynamo.Table('userTable') #userTable
+    response = usertable.get_item(Key={'username': shareTo})
+    if 'Item' not in response:
+        return False
+    try:
+        response = table.put_item(
+        Item={
+            'shareId': share_id,
+            'shareFrom': owner,
+            'filename': filename,
+            'shareTo': shareTo
+        },
+     )
+        return True
+    except Exception as e:
+        return False
+
+def list_files_shared_with_user(username):
+    dynamo = boto3.resource('dynamodb')
+    files = []
+    table = dynamo.Table('shareTable')
+    response = table.scan(
+        FilterExpression=Key('shareTo').eq(username)
+    )
+    for item in response['Items']:
+        files.append(item)
+    return files
+
+def convert_sharefile_to_list_files(sharefile):
+    s3 = boto3.client('s3')
+    file_list=[]
+    for data in sharefile:
+        file_owner = data['shareFrom']
+        filename = data['filename']
+        content = s3.head_object(Bucket = BUCKET_NAME, Key = file_owner + "/" + filename)
+        file_size = content["ContentLength"]
+        last_modified = content["LastModified"].strftime("%Y-%m-%d %H:%M:%S")
+        file_list.append({
+            "Key": filename,
+            "Size": file_size,
+            "LastModified": last_modified,
+            "owner": file_owner
+        })
+    return file_list
